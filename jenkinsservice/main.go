7754848edb625f0db694038a8409c6938f699c57
Jon Fast <jfast@acc.org>
2018-09-25 01:08:57 -0400

    added jenkins notifier

--- jenkinsservice/main.go
+++ jenkinsservice/main.go
@@ -7,16 +7,21 @@ import (
 	"net/http"
 )
 
+var configuration JenkinsConfiguration
+
 func main() {
 	utilities.CreateLog()
 	utilities.LogHeader("Jenkins Service")
 	utilities.LogApplicationStart()
 
-	// utilities.DecodeJsonFromFile("./appsettings.json", &configuration)
+	utilities.DecodeJsonFromFile("./appsettings.json", &configuration)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/Daemon/CreateUpdateJob", createUpdateJob).Methods(utilities.PostMethod)
-	router.HandleFunc("/Daemon/DeleteJob", deleteJob).Methods(utilities.PostMethod)
+	router.HandleFunc("/Daemon/CreateFolder", createFolder).Methods(utilities.PostMethod)
+	router.HandleFunc("/Daemon/DeleteJobOrFolder", deleteJobOrFolder).Methods(utilities.PostMethod)
+	router.HandleFunc("/Daemon/GetJenkinsMetadata", getJenkinsMetadata).Methods(utilities.PostMethod)
+	router.HandleFunc("/Daemon/GetJenkinsCrumb", getJenkinsCrumb).Methods(utilities.PostMethod)
 
 	localPort := utilities.GetLocalPort(1212) // TODO: Replace
 	utilities.LogContentService(localPort)
@@ -28,6 +33,18 @@ func createUpdateJob(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deleteJob(w http.ResponseWriter, r *http.Request) {
+func createFolder(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteJobOrFolder(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getJenkinsMetadata(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getJenkinsCrumb(w http.ResponseWriter, r *http.Request) {
 
 }
--- jenkinsservice/server.go
+++ jenkinsservice/server.go
@@ -1,1 +1,9 @@
 package main
+
+type JenkinsConfiguration struct {
+	Port               int    `json:"port"`
+	JenkinsUrl         string `json:"jenkinsUrl"`
+	Username           string `json:"username"`
+	AccessToken        string `json:"accessToken"`
+	FolderTemplatePath string `json:"folderTemplatePath"`
+}
