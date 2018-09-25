165c889f74e0e78861684e1ed3f91ae6ae7b9653
Jon Fast <jfast@acc.org>
2018-09-25 12:06:17 -0400

    added jenkins models and some server code

--- jenkinsservice/server.go
+++ jenkinsservice/server.go
@@ -1,9 +1,36 @@
 package main
 
+import "net/http"
+
 type JenkinsConfiguration struct {
 	Port               int    `json:"port"`
 	JenkinsUrl         string `json:"jenkinsUrl"`
 	Username           string `json:"username"`
 	AccessToken        string `json:"accessToken"`
 	FolderTemplatePath string `json:"folderTemplatePath"`
+}
+
+type JenkinsEndpoint struct {
+	configuration JenkinsConfiguration
+	client        http.Client
+}
+
+func (je *JenkinsEndpoint) CreateUpdateJob() {
+
+}
+
+func (je *JenkinsEndpoint) CreateFolder() {
+
+}
+
+func (je *JenkinsEndpoint) DeleteJobOrFolder() {
+
+}
+
+func (je *JenkinsEndpoint) GetJenkinsMetadata() {
+
+}
+
+func (je *JenkinsEndpoint) GetJenkinsCrumb() {
+
 }
