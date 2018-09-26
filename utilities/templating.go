1efd2e0386b9c7efbbcaf42e2a43533b3ca27dfb
Jon Fast <jfast@acc.org>
2018-09-25 22:12:49 -0400

    added templating code and an offline test tool

---
+++ utilities/templating.go
@@ -0,0 +1,19 @@
+package utilities
+
+import "github.com/aymerick/raymond"
+
+func RunTemplate(template string, input interface{}) (*string, error) {
+	tpl, err := raymond.Parse(template)
+	if err != nil {
+		return nil, err
+	}
+	result, err := tpl.Exec(input)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func RunTemplateFromFile(path string, input interface{}) (*string, error) {
+
+}
