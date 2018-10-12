01c02958dfe49a04b12c8e7731c242a2b5fc2028
Jon Fast <jfast@acc.org>
2018-10-11 22:52:30 -0400

    continued work on jenkins utils

--- jenkinsservice/server.go
+++ jenkinsservice/server.go
@@ -3,8 +3,12 @@ package main
 import (
 	"../models/jenkins"
 	"../utilities"
+	"../utilities/iteration"
 	"net/http"
+	"net/url"
+	"path"
 	"strconv"
+	"strings"
 )
 
 type JenkinsConfiguration struct {
@@ -46,16 +50,16 @@ func (je *JenkinsEndpoint) CreateUpdateJob(crumb jenkins.Crumb, request jenkins.
 	return result, nil
 }
 
-func (je *JenkinsEndpoint) CreateFolder(crumb jenkins.Crumb, request jenkins.JobRequest) (*string, error) {
+func (je *JenkinsEndpoint) CreateFolder(crumb jenkins.Crumb, request *jenkins.JobRequest) (*string, error) {
 	utilities.LogInfo("Create Jenkins Folder -> " + request.FolderUrl)
-	checkFrag := jenkins.GetJenkinsJobFolderUrlFragment(
-		je.configuration.JenkinsUrl, request.FolderUrl, false)
+
+	checkFrag := je.buildCreateUpdateCheckUrl(*request, true)
 	folderTemplate, err := utilities.RunTemplateFromFile(je.configuration.FolderTemplatePath, utilities.Empty{})
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest(utilities.PostMethod, request.FolderUrl /* folderContents */, nil)
+	req, err := http.NewRequest(utilities.PostMethod, checkFrag, strings.NewReader(*folderTemplate))
 	if err != nil {
 		return nil, err
 	}
@@ -72,9 +76,10 @@ func (je *JenkinsEndpoint) CreateFolder(crumb jenkins.Crumb, request jenkins.Job
 	return result, nil
 }
 
-func (je *JenkinsEndpoint) DeleteJobOrFolder(crumb jenkins.Crumb, request jenkins.JobRequest) (*string, error) {
+func (je *JenkinsEndpoint) DeleteJobOrFolder(crumb jenkins.Crumb, request *jenkins.JobRequest) (*string, error) {
 	utilities.LogInfo("Delete Jenkins Job/Folder -> " + request.FolderUrl)
-	req, err := http.NewRequest(utilities.PostMethod, request.FolderUrl, nil)
+
+	req, err := http.NewRequest(utilities.PostMethod, je.buildDeleteUrl(*request), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -93,8 +98,8 @@ func (je *JenkinsEndpoint) DeleteJobOrFolder(crumb jenkins.Crumb, request jenkin
 
 func (je *JenkinsEndpoint) GetJenkinsMetadata(crumb jenkins.Crumb) (*jenkins.JobMetadata, error) {
 	utilities.LogInfo("Get Jenkins Job Metadata")
-	metadataUrl := je.buildJobMetadataUrl()
-	request, err := http.NewRequest(utilities.GetMethod, metadataUrl, nil)
+
+	request, err := http.NewRequest(utilities.GetMethod, je.buildJobMetadataUrl(), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -112,6 +117,7 @@ func (je *JenkinsEndpoint) GetJenkinsMetadata(crumb jenkins.Crumb) (*jenkins.Job
 	// give parent meaningful data
 	result.Name = "Build Server"
 	result.Url = je.configuration.JenkinsUrl
+
 	utilities.LogInfoMultiline(
 		"Metadata retrieved: ",
 		"Num Top-Level Jobs: "+strconv.Itoa(len(result.Jobs)))
@@ -120,6 +126,7 @@ func (je *JenkinsEndpoint) GetJenkinsMetadata(crumb jenkins.Crumb) (*jenkins.Job
 
 func (je *JenkinsEndpoint) GetJenkinsCrumb() (*jenkins.Crumb, error) {
 	utilities.LogInfo("Get Jenkins Crumb")
+
 	request, err := http.NewRequest(utilities.GetMethod, je.buildCrumbUrl(), nil)
 	if err != nil {
 		return nil, err
@@ -148,6 +155,28 @@ func (je *JenkinsEndpoint) addAuthHeader(r *http.Request) {
 
 func addCrumbHeader(crumb jenkins.Crumb, r *http.Request) {
 	r.Header.Add(crumb.CrumbRequestField, crumb.Crumb)
+}
+
+func (je *JenkinsEndpoint) buildCreateUpdateCheckUrl(request jenkins.JobRequest, existenceCheck bool) string {
+	folderUrlNoBase := strings.Replace(request.FolderUrl, je.configuration.JenkinsUrl, "", -1)
+	pathParser := new(iteration.PathParser)
+	pathParser.SetActionSeries(folderUrlNoBase)
+	pathParser.RemoveLastNActions(2)
+	pathString := pathParser.GetPathString(false) + "/"
+	folderJobName := iteration.GetLastPathComponent(folderUrlNoBase)
+	var actionFragment string
+	if existenceCheck {
+		actionFragment = "checkJobName?value="
+	} else {
+		actionFragment = "createItem?name="
+	}
+	jenkinsFolderQuery := je.configuration.JenkinsUrl + "/" +
+		url.QueryEscape(pathString+actionFragment+folderJobName)
+	return jenkinsFolderQuery
+}
+
+func (je *JenkinsEndpoint) buildDeleteUrl(request jenkins.JobRequest) string {
+	return path.Join(je.configuration.JenkinsUrl, request.FolderUrl, "doDelete")
 }
 
 func (je *JenkinsEndpoint) buildCrumbUrl() string {
