a2cbe156f3cb912b3f853ba3829ed8cca0112b5f
Jon Fast <jfast@acc.org>
2018-10-12 21:52:33 -0400

    beginning boilerplate on persistence service including renaming

--- persistenceservice/server/server.go
+++ persistenceservice/server/server.go
@@ -14,3 +14,19 @@ func NewPersistenceServiceEndpoint(configuration PersistenceServiceConfiguration
 	result.Configuration = configuration
 	return result
 }
+
+func (p *PersistenceServiceEndpoint) SetKeyValueCache() {
+
+}
+
+func (p *PersistenceServiceEndpoint) GetKeyValueCache() {
+
+}
+
+func (p *PersistenceServiceEndpoint) GetInfrastructureMetadata() {
+
+}
+
+func (p *PersistenceServiceEndpoint) SetLogRecord() {
+
+}
