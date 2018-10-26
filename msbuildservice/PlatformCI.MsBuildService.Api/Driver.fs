1dd5d9ddf194635420d6f4b6def677ef86c5113f
Jon Fast <jfast@acc.org>
2018-10-25 20:02:47 -0400

    boilerplate.

---
+++ msbuildservice/PlatformCI.MsBuildService.Api/Driver.fs
@@ -0,0 +1,10 @@
+﻿module Driver
+
+open Suave.Successful
+open Suave.Http
+
+let getInfrastructureMetadata(req : HttpRequest) = OK("Something")
+let getEnvironmentData(req : HttpRequest) = OK("Something")
+let getFlattenedData(req : HttpRequest) = OK("Something")
+
+
--- msbuildservice/PlatformCI.MsBuildService.Api/Program.fs
+++ msbuildservice/PlatformCI.MsBuildService.Api/Program.fs
@@ -1,8 +1,45 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.Threading
 
-open System
+open Suave
+open Suave.Successful
+open Suave.Writers
+open Suave.Filters  
+open Suave.Operators
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _argv = 
+  let jsonMimeType = "application/json;charset=utf-8"
+  // let appSettingsFilePath = "./appsettings.json"
+
+  let cts = new CancellationTokenSource()
+  let conf = { 
+    defaultConfig with
+      bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 7999 ];
+      cancellationToken = cts.Token
+    }
+
+  let app = 
+    choose 
+        [ POST >=> choose 
+            [ path "/Daemon/GetInfrastructureMetadata" >=> 
+                request (fun req -> Driver.getInfrastructureMetadata(req)) >=> 
+                setMimeType jsonMimeType;
+              path "/Daemon/GetFlattenedInfrastructureMetadata" >=> 
+                request (fun req -> Driver.getFlattenedData(req)) >=> 
+                setMimeType jsonMimeType;
+              path "/Daemon/GetEnvironmentData" >=> 
+                request (fun req -> Driver.getEnvironmentData(req)) >=> 
+                setMimeType jsonMimeType;
+            ]
+        ]
+        
+  let _listening, server = startWebServerAsync conf app
+    
+  Async.Start(server, cts.Token)
+  printfn "Make requests now"
+  Console.ReadKey true |> ignore
+    
+  cts.Cancel()
+
+  0
