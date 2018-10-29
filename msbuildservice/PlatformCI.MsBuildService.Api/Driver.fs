e0dccd860e18aabfa8c120f53b3d67b22f2fd863
Jon Fast <jfast@acc.org>
2018-10-29 01:16:03 -0400

    got msbuild service into a workable format.

--- msbuildservice/PlatformCI.MsBuildService.Api/Driver.fs
+++ msbuildservice/PlatformCI.MsBuildService.Api/Driver.fs
@@ -5,18 +5,49 @@ open Suave.Http
 open System.Text
 open Newtonsoft.Json
 
-open PlatformCI.MsBuildService.Models
-open PlatformCI.MsBuildService.Driver
+open PlatformCI.MsBuildService.Driver.Implementation
+open PlatformCI.MsBuildService.Models.Implementation
 
-let microsoftBuildEndpoint = new DefaultMicrosoftBuildProviderEndpoint()
+let filesystemImpl = new BasicFilesystemProvider()
+let projectResolver = new HackedMicrosoftProjectResolver()
+let microsoftBuildEndpoint = new DefaultMicrosoftBuildProviderEndpoint(filesystemImpl, projectResolver)
 
-let getEnvironmentData(req : HttpRequest) = OK("Something")
-let getFlattenedData(req : HttpRequest) = OK("Something")
-
-let getInfrastructureMetadata = fun (req : Suave.Http.HttpRequest) ->
+let getProjectFromFileBytes = fun (req : Suave.Http.HttpRequest) ->
     let stringFormat = Encoding.UTF8.GetString(req.rawForm)
-    let requestFilterObject = JsonConvert.DeserializeObject<InfrastructureRequestFilter>(stringFormat)
-    let metadata = getEfContext.GetInfrastructureMetadata(requestFilterObject)
+    let payloadObject = JsonConvert.DeserializeObject<FilePayload>(stringFormat)
+    let metadata = microsoftBuildEndpoint.GetProjectFromFileBytes(payloadObject)
+    let json = metadata |> JsonConvert.SerializeObject
+    OK (json)
+
+let getSolutionFromFileBytes = fun (req : Suave.Http.HttpRequest) ->
+    let stringFormat = Encoding.UTF8.GetString(req.rawForm)
+    let payloadObject = JsonConvert.DeserializeObject<FilePayload>(stringFormat)
+    let metadata = microsoftBuildEndpoint.GetSolutionFromFileBytes(payloadObject)
+    let json = metadata |> JsonConvert.SerializeObject
+    OK (json)
+
+let getPublishProfileFromFileBytes = fun (req : Suave.Http.HttpRequest) ->
+    let stringFormat = Encoding.UTF8.GetString(req.rawForm)
+    let payloadObject = JsonConvert.DeserializeObject<FilePayload>(stringFormat)
+    let metadata = microsoftBuildEndpoint.GetPublishProfileFromFileBytes(payloadObject)
+    let json = metadata |> JsonConvert.SerializeObject
+    OK (json)
+
+let getProjectFromLocalPath = fun (req : Suave.Http.HttpRequest) ->
+    let stringFormat = Encoding.UTF8.GetString(req.rawForm)
+    let metadata = microsoftBuildEndpoint.GetProjectFromLocalPath(stringFormat)
+    let json = metadata |> JsonConvert.SerializeObject
+    OK (json)
+
+let getSolutionFromLocalPath = fun (req : Suave.Http.HttpRequest) ->
+    let stringFormat = Encoding.UTF8.GetString(req.rawForm)
+    let metadata = microsoftBuildEndpoint.GetSolutionFromLocalPath(stringFormat)
+    let json = metadata |> JsonConvert.SerializeObject
+    OK (json)
+
+let getPublishProfileFromLocalPath = fun (req : Suave.Http.HttpRequest) ->
+    let stringFormat = Encoding.UTF8.GetString(req.rawForm)
+    let metadata = microsoftBuildEndpoint.GetPublishProfileFromLocalPath(stringFormat)
     let json = metadata |> JsonConvert.SerializeObject
     OK (json)
 
