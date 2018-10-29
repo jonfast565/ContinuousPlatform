f6413e5871c9805d6d3cc377dfe015dfb8af9888
Jon Fast <jfast@acc.org>
2018-10-29 03:19:21 -0400

    partial deliverable builder implementation

--- clients/msbuildclient/msbuildclient.go
+++ clients/msbuildclient/msbuildclient.go
@@ -1,12 +1,19 @@
 package msbuildclient
 
 import (
-	"net/http"
+	"../../constants"
 	"../../jsonutil"
-	)
+	"../../models/miscmodel"
+	"../../models/projectmodel"
+	"../../webutil"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
 
 var (
-	SettingsFilePath = "./repoclient-settings.json"
+	SettingsFilePath = "./msbuildclient-settings.json"
 )
 
 type ClientConfiguration struct {
@@ -23,4 +30,97 @@ func NewMsBuildClient() MsBuildClient {
 	var config ClientConfiguration
 	jsonutil.DecodeJsonFromFile(SettingsFilePath, &config)
 	return MsBuildClient{configuration: config, client: http.Client{}}
+}
+
+func (msbc MsBuildClient) GetSolution(
+	payload miscmodel.FilePayload) (*projectmodel.MsBuildSolution, error) {
+	// build service url
+	myUrl := webutil.NewEmptyUrl()
+	myUrl.SetBase(constants.DefaultScheme,
+		msbc.configuration.Hostname,
+		strconv.Itoa(msbc.configuration.Port))
+	myUrl.AppendPathFragments([]string{"Daemon", "GetSolution", "Bytes"})
+
+	requestJson, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	// execute request
+	request, err := http.NewRequest(constants.PostMethod,
+		myUrl.GetUrlStringValue(),
+		bytes.NewReader(requestJson))
+	if err != nil {
+		return nil, err
+	}
+
+	var value projectmodel.MsBuildSolution
+	err = webutil.ExecuteRequestAndReadJsonBody(&msbc.client, request, &value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &value, nil
+}
+
+func (msbc MsBuildClient) GetProject(
+	payload miscmodel.FilePayload) (*projectmodel.MsBuildProject, error) {
+	// build service url
+	myUrl := webutil.NewEmptyUrl()
+	myUrl.SetBase(constants.DefaultScheme,
+		msbc.configuration.Hostname,
+		strconv.Itoa(msbc.configuration.Port))
+	myUrl.AppendPathFragments([]string{"Daemon", "GetProject", "Bytes"})
+
+	requestJson, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	// execute request
+	request, err := http.NewRequest(constants.PostMethod,
+		myUrl.GetUrlStringValue(),
+		bytes.NewReader(requestJson))
+	if err != nil {
+		return nil, err
+	}
+
+	var value projectmodel.MsBuildProject
+	err = webutil.ExecuteRequestAndReadJsonBody(&msbc.client, request, &value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &value, nil
+}
+
+func (msbc MsBuildClient) GetPublishProfile(
+	payload miscmodel.FilePayload) (*projectmodel.MsBuildPublishProfile, error) {
+	// build service url
+	myUrl := webutil.NewEmptyUrl()
+	myUrl.SetBase(constants.DefaultScheme,
+		msbc.configuration.Hostname,
+		strconv.Itoa(msbc.configuration.Port))
+	myUrl.AppendPathFragments([]string{"Daemon", "GetPublishProfile", "Bytes"})
+
+	requestJson, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	// execute request
+	request, err := http.NewRequest(constants.PostMethod,
+		myUrl.GetUrlStringValue(),
+		bytes.NewReader(requestJson))
+	if err != nil {
+		return nil, err
+	}
+
+	var value projectmodel.MsBuildPublishProfile
+	err = webutil.ExecuteRequestAndReadJsonBody(&msbc.client, request, &value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &value, nil
 }
--- models/repomodel/repomodel.go
+++ models/repomodel/repomodel.go
@@ -64,10 +64,15 @@ type RepositoryFileMetadata struct {
 	Repo   string
 	Branch string
 	Name   string
+	File   filesysmodel.FileSystemMetadata
 }
 
-func NewRepositoryFileMetadata(repo string, branch string, name string) RepositoryFileMetadata {
-	return RepositoryFileMetadata{Repo: repo, Branch: branch, Name: name}
+func NewRepositoryFileMetadata(
+	repo string,
+	branch string,
+	name string,
+	file filesysmodel.FileSystemMetadata) RepositoryFileMetadata {
+	return RepositoryFileMetadata{Repo: repo, Branch: branch, Name: name, File: file}
 }
 
 type RepositoryPackage struct {
