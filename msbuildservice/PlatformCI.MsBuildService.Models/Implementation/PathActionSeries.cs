b0927c7197f76b723fb0f3c28675e383b3aa6ebd
Jon Fast <jfast@acc.org>
2018-10-29 01:06:54 -0400

    added api service stuff.

--- msbuildservice/PlatformCI.MsBuildService.Models/Implementation/PathActionSeries.cs
+++ msbuildservice/PlatformCI.MsBuildService.Models/Implementation/PathActionSeries.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using PlatformCI.MsBuildService.Models.Abstractions;
 using PlatformCI.MsBuildService.Models.Interfaces;
 
 namespace PlatformCI.MsBuildService.Models.Implementation
 {
     public class PathActionSeries
     {
-        public PathActionSeries(ICollection<ISourceControlPathAction> actions)
+        public PathActionSeries(ICollection<SourceControlPathAction> actions)
         {
             Actions = actions;
         }
 
-        public ICollection<ISourceControlPathAction> Actions { get; }
+        public ICollection<SourceControlPathAction> Actions { get; }
+
+        public string GetLastItem()
+        {
+            return Actions.LastOrDefault()?.NextDirectory;
+        }
     }
 }
