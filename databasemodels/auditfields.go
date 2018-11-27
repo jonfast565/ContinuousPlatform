ac2489c03027a8c042a87c5ef4a3a7a421783855
Jon Fast <jfast@acc.org>
2018-11-27 15:40:46 -0500

    add version information, add logging statements back to msbuildservice.

--- databasemodels/auditfields.go
+++ databasemodels/auditfields.go
@@ -1,10 +1,12 @@
 package databasemodels
 
-import "time"
+import (
+	"time"
+)
 
 type AuditFields struct {
 	CreatedDateTime      time.Time `gorm:"not null;default:now()"`
-	CreatedBy            string    `gorm:"not null;default:'SystemCI'"`
+	CreatedBy            string    `gorm:"not null;default:'ContinuousPlatform'"`
 	LastModifiedDateTime time.Time `gorm:"not null;default:now()"`
-	LastModifiedBy       string    `gorm:"not null;default:'SystemCI'"`
+	LastModifiedBy       string    `gorm:"not null;default:'ContinuousPlatform'"`
 }
