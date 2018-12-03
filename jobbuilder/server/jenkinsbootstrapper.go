595c1c952de6bbb046123b4a47b8ff5027c00ee9
Jon Fast <jfast@acc.org>
2018-12-03 16:54:50 -0500

    scripts generate ok w/o publish profile. onto deployment with Jenkins

--- jobbuilder/server/jenkinsbootstrapper.go
+++ jobbuilder/server/jenkinsbootstrapper.go
@@ -4,6 +4,7 @@ import (
 	"../../clients/jenkinsclient"
 	"../../constants"
 	"../../logging"
+	"../../models/genmodel"
 	"../../models/jenkinsmodel"
 	"../../models/jobmodel"
 	"../../stringutil"
@@ -26,46 +27,17 @@ func DeployJenkinsJobs(details *jobmodel.JobDetails) {
 
 	jenkinsClient := jenkinsclient.NewJenkinsClient()
 	jenkinsMetadata, err := jenkinsClient.GetJenkinsMetadata()
-	jenkinsKeys := jenkinsMetadata.GetFlattenedKeys()
 	if err != nil {
 		panic(err)
 	}
 
-	myMetadataKeys := make(jenkinsmodel.JenkinsJobKeyList, 0)
-	for _, script := range scripts.Scripts {
-		isJenkinsScript := stringutil.StringArrayContains(script.ToolScope, constants.JenkinsToolName)
-		if !isJenkinsScript {
-			continue
-		}
-		var tempKeys []string
-		for i, key := range script.KeyElements {
-			tempKeys = append(tempKeys, key)
-			currentKeys := make([]string, 0)
-			currentKeys = append(currentKeys, tempKeys...)
-			if i == len(script.KeyElements)-1 {
-				if !myMetadataKeys.KeyAlreadyExists(currentKeys) {
-					myMetadataKeys = append(myMetadataKeys, jenkinsmodel.JenkinsJobKey{
-						Keys: script.KeyElements,
-						Type: string(jenkinsmodel.PipelineJob),
-					})
-				}
-			} else {
-				if !jenkinsKeys.PartialKeyAlreadyExists(currentKeys) &&
-					!myMetadataKeys.KeyAlreadyExists(currentKeys) {
-					myMetadataKeys = append(myMetadataKeys, jenkinsmodel.JenkinsJobKey{
-						Keys: currentKeys,
-						Type: string(jenkinsmodel.Folder),
-					})
-				}
-			}
-		}
-	}
-
-	sort.Sort(myMetadataKeys)
+	myMetadataKeys := buildKeyListFromScripts(scripts)
 	jenkinsInstanceMetadataKeys := jenkinsMetadata.GetFlattenedKeys()
 	edits := buildEditList(&myMetadataKeys, jenkinsInstanceMetadataKeys)
-	sort.Sort(edits)
+	persistEditList(edits, jenkinsClient)
+}
 
+func persistEditList(edits jenkinsmodel.JenkinsEditList, jenkinsClient jenkinsclient.JenkinsClient) {
 	for _, edit := range edits {
 		jobRequest := edit.GetJobRequest()
 		jobRequest.SanitizeSegments()
@@ -97,19 +69,37 @@ func DeployJenkinsJobs(details *jobmodel.JobDetails) {
 	}
 }
 
+func buildKeyListFromScripts(scripts *genmodel.ScriptPackage) jenkinsmodel.JenkinsJobKeyList {
+	myMetadataKeys := make(jenkinsmodel.JenkinsJobKeyList, 0)
+	for _, script := range scripts.Scripts {
+		isJenkinsScript := stringutil.StringArrayContains(script.ToolScope, constants.JenkinsToolName)
+		if !isJenkinsScript {
+			continue
+		}
+		keySet := script.GetJenkinsKeySet()
+		for _, key := range keySet {
+			if !myMetadataKeys.KeyAlreadyExists(key.Keys) {
+				myMetadataKeys = append(myMetadataKeys, key)
+			}
+		}
+	}
+	sort.Sort(myMetadataKeys)
+	return myMetadataKeys
+}
+
 func buildEditList(
 	l1 *jenkinsmodel.JenkinsJobKeyList,
 	l2 *jenkinsmodel.JenkinsJobKeyList) jenkinsmodel.JenkinsEditList {
 	var results jenkinsmodel.JenkinsEditList
 	for _, k1 := range *l1 {
 		result := linq.From(*l2).FirstWithT(func(key jenkinsmodel.JenkinsJobKey) bool {
-			return stringutil.StringArrayCompare(k1.Keys, key.Keys)
+			return stringutil.StringArrayCompare(k1.Keys, key.Keys) && k1.Type == key.Type
 		})
 
 		if result != nil {
 			// update
 			resultKey := result.(jenkinsmodel.JenkinsJobKey)
-			if resultKey.Type == string(jenkinsmodel.Folder) {
+			if resultKey.Type == jenkinsmodel.Folder {
 				continue
 			}
 			results = append(results, jenkinsmodel.JenkinsEdit{
@@ -119,10 +109,9 @@ func buildEditList(
 			})
 		} else {
 			// insert
-			if k1.Type == string(jenkinsmodel.Folder) {
+			if k1.Type == jenkinsmodel.Folder {
 				results = append(results, jenkinsmodel.JenkinsEdit{
 					Keys:     k1.Keys,
-					Contents: "",
 					EditType: jenkinsmodel.AddFolder,
 				})
 			} else {
@@ -144,11 +133,11 @@ func buildEditList(
 			resultKey := result.(jenkinsmodel.JenkinsJobKey)
 			results = append(results, jenkinsmodel.JenkinsEdit{
 				Keys:     resultKey.Keys,
-				Contents: "",
 				EditType: jenkinsmodel.RemoveJobFolder,
 			})
 		}
 	}
 
+	sort.Sort(results)
 	return results
 }
