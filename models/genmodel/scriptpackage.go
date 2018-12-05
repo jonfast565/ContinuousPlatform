f46eb74fbdb5fd47a31a4e2ca84b09cb4c4ea587
Jon Fast <jfast@acc.org>
2018-12-04 20:40:23 -0500

    put scripts into the stuff

--- models/genmodel/scriptpackage.go
+++ models/genmodel/scriptpackage.go
@@ -1,5 +1,27 @@
 package genmodel
 
+import (
+	"../../stringutil"
+	"../jenkinsmodel"
+	"github.com/ahmetb/go-linq"
+)
+
 type ScriptPackage struct {
 	Scripts []ScriptKeyValuePair
+}
+
+func (sp ScriptPackage) GetScriptContentsByKey(key jenkinsmodel.JenkinsJobKey) *string {
+	for _, script := range sp.Scripts {
+		myKeys := script.GetJenkinsKeyList()
+		myKeys.SanitizeKeyList()
+		result := linq.From(myKeys).FirstWithT(func(myKey jenkinsmodel.JenkinsJobKey) bool {
+			return stringutil.StringArrayCompare(key.Keys, myKey.Keys) &&
+				myKey.Type == jenkinsmodel.PipelineJob
+		})
+
+		if result != nil {
+			return &script.Value
+		}
+	}
+	return nil
 }
