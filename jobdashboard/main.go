1f8fe0849f74ad064c1b0faaca3cbf497dad83e7
Jon Fast <jfast@acc.org>
2018-12-07 23:38:58 -0500

    added gocui starter code

--- jobdashboard/main.go
+++ jobdashboard/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	_ "github.com/marcusolsson/tui-go"
+	"fmt"
+	"github.com/jroimartin/gocui"
+	"log"
 )
 
 func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
 
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
