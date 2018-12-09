e7a36e387b16b02625fb2e065b684356fd4668b6
Jon Fast <jfast@acc.org>
2018-12-08 21:08:43 -0500

    better but still sucking

--- fileutil/filewriter.go
+++ fileutil/filewriter.go
@@ -3,7 +3,7 @@ package fileutil
 import "io/ioutil"
 
 func WriteFile(path string, contents string) {
-	err := ioutil.WriteFile(path, []byte(contents), 0666)
+	err := ioutil.WriteFile(path, []byte(contents), 0777)
 	if err != nil {
 		panic(err)
 	}
